04e0f72f94bfde8fa84519e346ae781c818ea2ad
Jessica Bealey <jfbealey@gmail.com>
2023-05-11 14:55:51 +0100

    count words challenge complete

--- lib/count_words.rb
+++ lib/count_words.rb
@@ -1,4 +1,5 @@
 def  count_words(str)
     str_arr = str.split(" ")
-    str_arr.length
+    count = str_arr.length
+    "Number of words in string: #{count}"
 end
--- spec/count_words_spec.rb
+++ spec/count_words_spec.rb
@@ -1,10 +1,10 @@
 require 'count_words'
 
 RSpec.describe "count_words method" do
-    it "returns 0 if string empty" do
-        expect(count_words("")).to eq 0
+    it "returns count of 0 if string empty" do
+        expect(count_words("")).to eq "Number of words in string: 0"
     end
     it "returns num of word in string" do
-        expect(count_words("Me and Afrika are coding")).to eq 5
+        expect(count_words("Me and Afrika are coding")).to eq "Number of words in string: 5"
     end
 end
