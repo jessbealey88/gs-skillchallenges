c8aeb556b668c1e00e4c53f3ad754e2f021dcf55
Jessica Bealey <jfbealey@gmail.com>
2023-05-16 23:31:57 +0100

    Debugging exercise complete

---
+++ lib/debugging_vowel_remover.rb
@@ -0,0 +1,25 @@
+# File: lib/debugging_vowel_remover.rb
+
+class VowelRemover
+    def initialize(text)
+      @text = text
+      @vowels = ["a", "e", "i", "o", "u"]
+    end
+  
+    def remove_vowels()
+      i = 0
+      while i < @text.length()
+        #binding.irb
+        if @vowels.include? @text[i].downcase
+          @text = @text.slice(0,i) + @text.slice(i+1..-1)
+        else
+            i += 1
+        #binding.irb
+        end
+      end
+      return @text
+    end
+  end
+  
+  remover =VowelRemover.new("aeiou")
+  remover.remove_vowels()
